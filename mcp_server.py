fc25860ad5a6a15f189be29c66a2803fe8ddc6b0
Alastair Plant <alastair.plant@answerdigital.com>
2026-03-12 14:21:02 +0000

    Initial commit.

---
+++ mcp_server.py
@@ -0,0 +1,38 @@
+
+"""
+Demo SQLite MCP in Python
+https://www.reddit.com/r/AI_Agents/comments/1jd9gzv/learn_mcp_by_building_an_sqlite_ai_agent/
+https://github.com/prayanks/mcp-sqlite-server/blob/main/sqlite_sdio_mcp_server.py
+"""
+
+import sqlite3
+from setup import setup_db
+
+# https://gofastmcp.com/getting-started/welcome
+from mcp.server.fastmcp import FastMCP
+
+
+setup_db('answer', 'capabilities')
+from loguru import logger
+
+
+
+mcp = FastMCP("SQL Agent Server")
+
+@mcp.tool()
+def query_data(sql: str) -> str:
+    """Execute SQL queries safely."""
+    logger.info(f"Executing SQL query: {sql}")
+    conn = sqlite3.connect("./answer.db")
+    try:
+        result = conn.execute(sql).fetchall()
+        conn.commit()
+        return "\n".join(str(row) for row in result)
+    except Exception as e:
+        return f"Error: {str(e)}"
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    print("Starting server...")
+    mcp.run(transport="stdio")
---
+++ setup.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def setup_db(db_name, table_name):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS ''' + table_name + '''  
+                      (id INTEGER PRIMARY KEY, name TEXT)''')
+    connection.commit()
+    connection.close()
