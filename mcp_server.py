ef907ff769b16edd97dc3a3f228e71d13fcd6a23
Alastair Plant <alastair.plant@answerdigital.com>
2026-03-16 09:09:50 +0000

    Added link to guide to give Ollama models access to the MCP server.

--- mcp_server.py
+++ mcp_server.py
@@ -1,6 +1,8 @@
 
 """
 Demo SQLite MCP in Python
+https://github.com/arjunprabhulal/mcp-llama3-client/blob/main/mcp_flight_client.py
+
 https://www.reddit.com/r/AI_Agents/comments/1jd9gzv/learn_mcp_by_building_an_sqlite_ai_agent/
 https://github.com/prayanks/mcp-sqlite-server/blob/main/sqlite_sdio_mcp_server.py
 https://gofastmcp.com/getting-started/welcome
